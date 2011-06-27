78dbf0fec3b1f75c9835aaace4244134b9ab25b1
Tiago Cunha <tiagovtg@gmail.com>
2011-06-27 11:53:35 -0300

    heroku

--- config/application.rb
+++ config/application.rb
@@ -43,8 +43,8 @@ module Sgss
     #    config.action_view.javascript_expansions[:defaults] = ['jquery-1.5.1.min', 'script', 'rails']
 
     # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "ISO-8859-1"
-    #    config.encoding = "utf-8"
+    #    config.encoding = "ISO-8859-1"
+    config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
