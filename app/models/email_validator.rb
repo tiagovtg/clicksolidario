fd5fdfb6beaef249c1b627a62869c79a93142806
Tiago Cunha <tiagovtg@gmail.com>
2011-06-27 11:02:13 -0300

    heroku

--- app/models/email_validator.rb
+++ app/models/email_validator.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     record.errors[attribute] << (options[:message] || "não é um email") unless
